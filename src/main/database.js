911bb555d8ea2f02c8bcc4a3b619aaed57c232a9
TMRafaelV <moraisv0271@gmail.com>
2025-11-09 01:20:35 -0300

    Reapply "up"

--- src/main/database.js
+++ src/main/database.js
@@ -1,16 +1,12 @@
-// src/main/database.js
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function addTeamPokemon(pokemon) {
-  // Verifica quantos pokémon já existem no time (máximo 3 para renderizar)
-  const teamCount = await prisma.teamPokemon.count();
-  
   return await prisma.teamPokemon.create({
     data: {
       uuid: pokemon.uuid,
       name: pokemon.name,
-      level: pokemon.level || 1,
+      level: pokemon.level,
       xp: pokemon.xp || 0,
       hp: pokemon.hp,
       maxHp: pokemon.maxHp,
@@ -22,38 +18,8 @@ async function addTeamPokemon(pokemon) {
   });
 }
 
-async function getTeamPokemon(id) {
-  return await prisma.teamPokemon.findUnique({
-    where: { id }
-  });
+async function getTeamPokemon() {
+  return await prisma.teamPokemon.findMany();
 }
 
-async function getAllTeamPokemon() {
-  // Retorna os 3 primeiros pokémon do time (os que são renderizados)
-  return await prisma.teamPokemon.findMany({
-    where: { id: { lte: 3 } },
-    orderBy: { id: 'asc' }
-  });
-}
-
-async function updateTeamPokemon(id, data) {
-  return await prisma.teamPokemon.update({
-    where: { id },
-    data
-  });
-}
-
-async function getTeamCount() {
-  return await prisma.teamPokemon.count({
-    where: { id: { lte: 3 } }
-  });
-}
-
-module.exports = { 
-  prisma, 
-  addTeamPokemon, 
-  getTeamPokemon,
-  getAllTeamPokemon,
-  updateTeamPokemon,
-  getTeamCount
-};
+module.exports = { addTeamPokemon, getTeamPokemon };
--- src/main/overlayPreload.js
+++ src/main/overlayPreload.js
@@ -1,16 +1,5 @@
-// src/main/overlayPreload.js
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge } = require("electron");
 
 contextBridge.exposeInMainWorld("overlayAPI", {
-  // Busca os pokémon do time (banco de dados)
-  getTeamPokemon: () => ipcRenderer.invoke("get-team-pokemon"),
-  
-  // Busca todos os pokémon disponíveis (para spawn)
-  getAllPokemon: () => ipcRenderer.invoke("read-all-pokemon"),
-  
-  // Salva um novo pokémon no banco
-  savePokemon: (pokemon) => ipcRenderer.invoke("save-pokemon", pokemon),
-  
-  // Atualiza dados de um pokémon existente
-  updatePokemon: (pokemonData) => ipcRenderer.invoke("update-pokemon", pokemonData),
+  getPokemonSprite: () => "./pikachu.png", // sprite de teste local
 });
